1644db5025da70979d3f402c3e14d608c5c9855a
Mateus Moreira <contatoihgs@gmail.com>
2020-03-26 22:14:05 -0300

    desafios 35-45 done

---
+++ desafios/desafio37.py
@@ -0,0 +1,31 @@
+""" 25/03/2020 jan Mesu
+
+Escreva um programa que peça um número inteiro qualquer
+e peça para o usuário escrever uma base de conversão.
+
+1 para Binários
+2 para octal
+3 para hexadecimal
+"""
+
+select = int(input("""
+Escolha uma base numéria para conversão
+
+1 - Para binários
+
+2 - Para octal
+
+3 - Para Hexadecimal
+
+        Escolha um número # """))
+
+answer = int(input('Escreva um número para converter na base escolhida $ '))
+
+
+if select == 1:
+   answer = bin(answer)
+elif select == 2:
+   answer = oct(answer)
+elif select == 3:
+   answer = hex(answer)    
+print(answer)
